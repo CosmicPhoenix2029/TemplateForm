52c85230f8148cd663150a1eb1067b3332c3be59
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-06 14:47:38 +0100

    added http methods

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url: string = 'https://putsreq.com/OJVzX0CVnL4UHZiMx0xO';
+  constructor(private http: HttpClient) { }
 
-  postUserSettingsForm(userSettings: UserSettings) {
-      
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
   }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    //the url is a placeholder until the http server is configured
+    return this.http.post(this.url, userSettings);  
+  };
 }
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -17,15 +19,40 @@ export class UserSettingsFormComponent {
     notes: 'here are some notes...'
   };
 
+  constructor(private dataService: DataService) { }
+
   userSettings: UserSettings = {...this.originalUserSettings}; 
+  postError: boolean = false;
+  postErrorMessage: string = '';
+  subscriptionTypes!: Observable<string[]>;
+
+  ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();  
+  }
 
   onBlur(field: NgModel) {
     console.log(`the name entered is: ${field.valid}`);      
   }
 
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
+
   onSubmit(form: NgForm) {
-    //if the form is valid, then log to the console
-    //this would normally then send to DB via API 
     console.log(`Form validation status: ${form.valid}`); 
+
+    if(form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings)
+      .subscribe({
+        next: result => console.log('success: ', result),
+        error: error => this.onHttpError(error)
+      });
+    }
+    else {
+      this.postError = true;
+      this.postErrorMessage = "please fix the errors above before re-submitting"
+    }
   }
 }
