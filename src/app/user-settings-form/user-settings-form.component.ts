eec9e5705033b83eda806e02e6d4fdfccad592fb
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-06 12:24:32 +0100

    added some validation

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class UserSettingsFormComponent {
 
+  originalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: false,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+  userSettings: UserSettings = {...this.originalUserSettings}; 
+
+  onSubmit(form: NgForm) {
+    //if the form is valid, then log to the console
+    //this would normally then send to DB via API 
+    console.log(`Form validation status: ${form.valid}`); 
+  }
 }
